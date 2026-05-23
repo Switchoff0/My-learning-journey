a0a12c7abb9ef7d7066680cb61c3abf9968ae909
Switchoff0 <moviesfree0099@gmail.com>
2026-05-23 23:05:19 +0530

    started js;fcc-lab and workshop ,some minor changes, file organise

---
+++ Javascript/fcc-Lab/lab-javascript-trivia-bot.js
@@ -0,0 +1,18 @@
+** start of script.js **
+
+console.log("Hello! I'm your coding fun fact guide!");
+let botName = "Funbot";
+let botLocation = "metro";
+let favoriteLanguage = "Javascript";
+console.log("My name is " + botName + " and I live on " + botLocation + ".");
+console.log("My favorite programming language is " + favoriteLanguage + ".");
+let codingFact = "Funfact about " + favoriteLanguage + " is that it is not my favorite language.";
+console.log(codingFact);
+codingFact = "if i have to state a fact then, " + favoriteLanguage + " is very precise not like Html.";
+console.log(codingFact);
+codingFact = botName + " this is  boring but i habe to include " + favoriteLanguage + ".";
+console.log(codingFact);
+console.log("It was fun sharing these facts with you. Goodbye! - " + botName + " from " + botLocation + ".");
+
+** end of script.js **
+
--- Javascript/note.js
+++ Javascript/note.js
@@ -1,1 +1,32 @@
-console.log(3);
+console.log(3); // Number
+console.log(3.3); //floting-point value
+console.log("hello world 2 &^%") // string
+console.log('hello world 1')// string
+let age = 22; // age is variable, 22 is value, data-type is Number ie 22
+console.log(age);
+age = 22.5;
+console.log(age);
+let x = 22; //bad variable name
+//bad variable name "let 2age = 23" variable can not start with Nnumber
+// valid variable name 
+let _age = 22
+console.log(_age);
+let $age = 20;
+console.log($age);
+let Age = 23;
+console.log(Age);// Age and age re different variable
+// use naming convention lile camelCase
+let thisIsCamelCase;//this is undifined variable
+const score = 100;
+console.log(score);
+//score = 150; now if you reassing the variable it will show syntex error
+let firstName = "switch";
+let lastName = "off";
+let fullName = firstName + " " + lastName
+console.log(fullName);
+fullName = firstName + lastName;
+console.log(fullName);
+let stg = "i";
+let stgi = "o";
+let string = stg.concat(stgi);
+console.log(string);
